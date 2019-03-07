29f8a768d9587a1b45652de160e7d8a83d63b5bd
AjiYakin <ajiyakin91@gmail.com>
2019-03-07 10:24:44 +0700

    Add go routine to update healthiness

--- cmd/gohealthz/main.go
+++ cmd/gohealthz/main.go
@@ -4,13 +4,18 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"time"
 
 	"github.com/ajiyakin/gohealthz/internal/pkg/handler"
 	"github.com/ajiyakin/gohealthz/internal/pkg/storage"
+	"github.com/ajiyakin/gohealthz/internal/pkg/updater"
 )
 
 func main() {
+	http.DefaultClient.Timeout = 800 * time.Millisecond
 	database := storage.NewInMemoryDatabase()
+
+	updater.StartUpdate(database, 5*time.Minute)
 
 	ui := http.FileServer(http.Dir(path.Join("web", "static")))
 	http.Handle("/", ui)
